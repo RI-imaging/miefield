22db7880e727bad86873f767d092ae9c5ec2eb55
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2015-02-26 15:26:24 +0100

    sphere FDTD data

--- examples/Sphere_FDTDcompare.py
+++ examples/Sphere_FDTDcompare.py
@@ -17,13 +17,13 @@ import miefield
 
 os.chdir(dirname(abspath(__file__)))
 
-zf = zipfile.ZipFile("Cylinder_FDTD.zip")
+zf = zipfile.ZipFile("Sphere_FDTD.zip")
 
 radius = 5.0 # radius of the cylinder in wavelengths
 nmed = 1.333 # refractive index of surrounding medium
 ncyl = 1.34 # refractive index of the cylinder
 lD = 6.0 # distance from center of cylinder to planar detector
-res = 42.0 # pixels per vacuum wavelength at the detector line
+res = 13.0 # pixels per vacuum wavelength at the detector line
 
 # FDTD
 fdtd_real = np.loadtxt(zf.open("fdtd_real.txt"))
@@ -32,24 +32,30 @@ fdtd = fdtd_real + 1j*fdtd_imag
 
 size = fdtd.shape[0] # pixel number of the planar detector
 
+## TODO:
 # Mie computation
-mie = miefield.GetFieldCylinder(radius, nmed, ncyl, lD, size, res)
-mie_bg = miefield.GetFieldCylinder(radius, nmed, nmed, lD, size, res)
-mie /= mie_bg
+#mie = miefield.GetFieldSphere(radius, nmed, ncyl, lD, size, res)
+#mie_bg = miefield.GetFieldSphere(radius, nmed, nmed, lD, size, res)
+#mie /= mie_bg
 
-fig, axes = plt.subplots(1,3)
+fig, axes = plt.subplots(2,3)
 
-axes[0].plot(np.arange(size), np.angle(mie), label="Mie phase")
-axes[0].plot(np.arange(size), np.angle(fdtd), label="FDTD phase")
+axes = axes.flatten()
 
-axes[1].plot(np.arange(size), mie.imag, label="Mie imag")
-axes[1].plot(np.arange(size), fdtd_imag, label="FDTD imag")
+## FDTD plots
+axes[0].imshow(np.angle(fdtd))
+axes[0].set_title("FDTD phase")
 
-axes[2].plot(np.arange(size), mie.real, label="Mie real")
-axes[2].plot(np.arange(size), fdtd_real, label="FDTD real")
+axes[1].imshow(fdtd_imag)
+axes[1].set_title("FDTD imag")
+
+axes[2].imshow(fdtd_real)
+axes[2].set_title("FDTD real")
+
+## Mie plots
+axes[3].set_title("Mie phase")
+axes[4].set_title("Mie imag")
+axes[5].set_title("Mie real")
 
 
-axes[0].legend(loc=3)
-axes[1].legend(loc=3)
-axes[2].legend(loc=3)
 plt.show()
