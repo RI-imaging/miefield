8aeac42a02836fd6dc2b2811a9df02be444f457a
Paul Müller <paul.mueller@biotec.tu-dresden.de>
2016-01-17 22:42:31 +0100

    transfer to RI-imaging org

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
         name=name,
         author=author,
         author_email='paul.mueller at biotec.tu-dresden.de',
-        url='http://paulmueller.github.io/miefield/',
+        url='http://RI-imaging.github.io/miefield/',
         version=version,
         packages=[name],
         package_dir={name: name},
